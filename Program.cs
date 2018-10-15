e86beb82c18472c9a33a582c732eb5eecb513cac
Ellekwis <43903852+Ellekwis@users.noreply.github.com>
2018-10-15 18:03:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Артём
+ * Date: 19.09.2018
+ * Time: 19:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Лаб1_Ном1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			// TODO: Implement Functionality Here
+			
+			for(int i=1 ; i<10 ; i++)
+				for(int j=1 ; j<10 ; j++)
+					Console.Write(i.ToString()+"*"+j.ToString()+"="+Convert.ToString(i*j)+"\t");
+			Console.WriteLine();
+			Console.ReadKey ();
+		}
+	}
+}
